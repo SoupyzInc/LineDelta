38cc5659a505e4ccd8103e266e7c7c3a4d814c65
Matthew Okashita <okashita.matthew@gmail.com>
2021-10-26 07:25:42 -0400

    Reintegrate non-scaled ASAE method
    There was a need to use the non-scaled ASAE method again for testing, as such, it was added back.

--- linespike/linespike.py
+++ linespike/linespike.py
@@ -14,6 +14,60 @@ class LineSpike:
     @staticmethod
     def asae(data, w=2):
         """
+        Finds the average slopes around all relative extrema of a data set.
+
+        Parameters
+        ----------
+        data : list of int
+            The data set to be analyzed.
+        w : int, optional
+            The amount of data points left and right of the extrema to be considered.
+        Returns
+        -------
+        int
+            The average slopes of all relative extrema of the data set.
+        """
+
+        if w < 1:
+            raise ValueError('Window size, w, must be greater than 0.')
+
+        # Find extrema
+        extrema_indices = []
+        for i in range(1, len(data) - 1):
+            if data[i - 1] > data[i] and data[i] < data[i + 1]:
+                extrema_indices.append(i)
+            elif data[i - 1] < data[i] and data[i] > data[i + 1]:
+                extrema_indices.append(i)
+
+        if len(extrema_indices) == 0:
+            return 0
+
+        # Iterate through all extrema (windows).
+        slope_sum = 0
+        for i in range(0, len(extrema_indices)):
+            # Find slope of window
+            window_sum = 0
+
+            # Set window indices
+            left_bound = extrema_indices[i] - w
+            if left_bound < 0:
+                left_bound = 0
+
+            right_bound = extrema_indices[i] + w
+            if right_bound > len(data) - 1:
+                right_bound = len(data) - 1
+
+            # Sum slopes of window
+            for x in range(left_bound, right_bound):
+                window_sum += abs(data[x + 1] - data[x])
+
+            slope_sum += window_sum / (right_bound - left_bound)
+
+        return slope_sum / len(extrema_indices)
+
+    @staticmethod
+    def asae_scaled(data, w=2):
+        """
         Finds the average slopes around all relative extrema of a data set converted to z-scores.
 
         Parameters
