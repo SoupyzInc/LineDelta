c84fe4fd1b4e889c218442fe5fc1f1fe7c59308e
Matthew Okashita <okashita.matthew@gmail.com>
2022-01-18 20:12:29 -0500

    Implement quicksort algorithm
    A working implementation of the quicksort algorithm has been added to sort the extrema of the data set.

--- linespike/linespike.py
+++ linespike/linespike.py
@@ -1,34 +1,62 @@
 import json
 import os
-import statistics
 
 
 class LineSpike:
     """
-    A class to hold all methods to perform an analysis of a line chart's 'spikiness,' using the LineSpike framework.
+    A class to hold all methods to perform an analysis of a time series' spikiness, using the LineSpike framework.
 
     Methods
     -------
-    asae(self, data, w)
-        Finds the average slopes around all relative extrema of a data set.
-    asae_scaled(self, data, w)
-        Finds the average slopes around all relative extrema of a data set converted to z-scores.
+    setup(self, location, file_name)
+        Sets up the LineSpike environment and runs the LineSpike framework.
+    get_data(self, location, file_name)
+        Loads the data sets specified in setup().
+    find_extrema(self)
+        Finds all local extrema in the data set, including boundaries.
+    partition(self, arr, low, high)
+        Partitions and sorts a data set for quicksort.
+    quicksort(self, arr, low, high)
+        An implementation of the quicksort algorithm.
+    lower_star_filtration(self)
+        Implements lower-star filtration on the data set to generate a merge tree and persistence diagram.
+    generate_persistence_diagram(self)
+        Generates a persistence diagram from the lower-star filtration.
     """
 
     data = []
+    extrema = []
     C = []
-    E = []
 
-    def setup(self, location):
-        self.get_data(location)
+    def setup(self, location, file_name):
+        # Load data
+        self.get_data(location, file_name)
+
+        # Isolate extrema
         self.find_extrema()
-        print(self.E)
-        self.sort_extrema(0, int(len(self.data) / 2))
-        print(self.E)
+        print('\nLoaded extrema -------------------')
+        print(self.extrema)
+
+        # Sort extrema
+        self.quicksort(self.extrema, 0, len(self.extrema) - 1)
+        print('\nSorted extrema -------------------')
+        print(self.extrema)
+
+        # Check sort
+        failed = False
+        for i in range(0, len(self.extrema) - 2):
+            if self.extrema[i][0] > self.extrema[i + 1][0]:
+                print('FAIL: ' + str(self.extrema[i][0]) + ' is > ' + str(self.extrema[i + 1][0]))
+                failed = True
+                break
+
+        if not failed:
+            print('SUCCESS')
+
         # self.lower_star_filtration()
         # self.generate_persistence_diagram()
 
-    def get_data(self, location):
+    def get_data(self, location, file_name):
         """
         Loads a specified data set.
 
@@ -39,60 +67,72 @@ class LineSpike:
         """
 
         location = os.getcwd() + '\\data\\' + location
+        # for file in os.listdir(location):
+        #     try:
+        #         if file.endswith(".json"):
+        #             print(file)
+        #             with open(location + '\\' + file) as f:
+        #                 self.data.append(json.load(f))
+        #     except Exception as e:
+        #         raise e
+
         for file in os.listdir(location):
             try:
-                if file.endswith(".json"):
+                if file.endswith(file_name + ".json"):
+                    print(file)
                     with open(location + '\\' + file) as f:
                         self.data.append(json.load(f))
             except Exception as e:
                 raise e
+
+        self.data = self.data[0]
 
     def find_extrema(self):
         """
         Finds all local extrema in the data set, including boundaries.
         """
 
-        self.E.append([self.data[0], 0])
+        self.extrema.append([self.data[0], 1])
 
+        c = 2
         for i in range(1, len(self.data) - 2):
-            if self.data[i-1] < self.data[i] and self.data[i+1] < self.data[i]:  # local max
-                self.E.append([self.data[i], i])
-            elif self.data[i-1] > self.data[i] and self.data[i+1] > self.data[i]:  # local min
-                self.E.append([self.data[i], i])
+            if self.data[i - 1] < self.data[i] and self.data[i + 1] < self.data[i]:  # local max
+                self.extrema.append([self.data[i], c])
+                c += 1
+            elif self.data[i - 1] > self.data[i] and self.data[i + 1] > self.data[i]:  # local min
+                self.extrema.append([self.data[i], c])
+                c += 1
 
-        self.E.append([self.data[-1], len(self.data) - 1])
+        self.extrema.append([self.data[-1], c])
 
     def partition(self, arr, low, high):
-        i = (low - 1)  # index of smaller element
-        pivot = arr[high]  # pivot
+        """
+        Partitions and sorts a data set for quicksort.
+        """
+
+        i = (low - 1)
+        pivot = arr[high][0]
 
         for j in range(low, high):
-
-            # If current element is smaller than or
-            # equal to pivot
-            if arr[j] <= pivot:
-                # increment index of smaller element
+            if arr[j][0] <= pivot:
                 i = i + 1
                 arr[i], arr[j] = arr[j], arr[i]
 
         arr[i + 1], arr[high] = arr[high], arr[i + 1]
         return i + 1
 
-    def sort_extrema(self, low, high):
+    def quicksort(self, arr, low, high):
         """
-        Sorts extrema in a data set from low to high y-values.
+        An implementation of the quicksort algorithm.
         """
 
-        if len(self.data) == 1:
-            return self.data
+        if len(arr) == 1:
+            return arr
         if low < high:
-            # pi is partitioning index, arr[p] is now
-            # at right place
-            pi = self.partition(self.data, low, high)
+            pi = self.partition(arr, low, high)
 
-            # Separately sort elements before
-            # partition and after partition
-            self.sort_extrema(low, pi - 1)
+            self.quicksort(arr, low, pi - 1)
+            self.quicksort(arr, pi + 1, high)
 
     def lower_star_filtration(self):
         """
@@ -104,9 +144,11 @@ class LineSpike:
         Generates a persistence diagram from the lower-star filtration.
         """
 
+
 def main():
     ls = LineSpike()
-    ls.setup('astro')
+    ls.setup('chi_homicide', 'weekly')
+
 
 if __name__ == '__main__':
     main()
