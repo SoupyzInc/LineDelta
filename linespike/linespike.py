161a9b16f7a3819219d0ecef6a2aa86c995bf11d
Matthew Okashita <okashita.matthew@gmail.com>
2022-01-19 00:01:53 -0500

    Implement new persistence diagram generation
    Using new method more similar to those discussed in Edels 2008 to generate the persistence diagrams.

--- linespike/linespike.py
+++ linespike/linespike.py
@@ -1,5 +1,14 @@
 import json
+import math
 import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import sparse
+import time
+
+from ripser import ripser
+from persim import plot_diagrams
 
 
 class LineSpike:
@@ -27,36 +36,39 @@ class LineSpike:
     data = []
     extrema = []
     C = []
+    file_output_name = ''
 
-    def setup(self, location, file_name):
+    def setup(self, location):
         # Load data
-        self.get_data(location, file_name)
+        self.get_data(location)
 
-        # Isolate extrema
-        self.find_extrema()
-        print('\nLoaded extrema -------------------')
-        print(self.extrema)
-
-        # Sort extrema
-        self.quicksort(self.extrema, 0, len(self.extrema) - 1)
-        print('\nSorted extrema -------------------')
-        print(self.extrema)
-
-        # Check sort
-        failed = False
-        for i in range(0, len(self.extrema) - 2):
-            if self.extrema[i][0] > self.extrema[i + 1][0]:
-                print('FAIL: ' + str(self.extrema[i][0]) + ' is > ' + str(self.extrema[i + 1][0]))
-                failed = True
-                break
-
-        if not failed:
-            print('SUCCESS')
+        # print(self.data)
+        #
+        # # Isolate extrema
+        # self.find_extrema()
+        # print('\nLoaded extrema -------------------')
+        # print(self.extrema)
+        #
+        # # Sort extrema
+        # self.quicksort(self.extrema, 0, len(self.extrema) - 1)
+        # print('\nSorted extrema -------------------')
+        # print(self.extrema)
+        #
+        # # Check sort
+        # failed = False
+        # for i in range(0, len(self.extrema) - 2):
+        #     if self.extrema[i][0] > self.extrema[i + 1][0]:
+        #         print('FAIL: ' + str(self.extrema[i][0]) + ' is > ' + str(self.extrema[i + 1][0]))
+        #         failed = True
+        #         break
+        #
+        # if not failed:
+        #     print('SUCCESS')
 
         # self.lower_star_filtration()
         # self.generate_persistence_diagram()
 
-    def get_data(self, location, file_name):
+    def get_data(self, location):
         """
         Loads a specified data set.
 
@@ -64,28 +76,38 @@ class LineSpike:
         ----------
         location : string
             The folder name of the data set under tests/data.
+        file_name : string
+            The ending of the specified file name(s) wanted.
         """
 
-        location = os.getcwd() + '\\data\\' + location
+        # location = os.getcwd() + '\\data\\' + location
         # for file in os.listdir(location):
         #     try:
-        #         if file.endswith(".json"):
+        #         if file.endswith(file_name + ".json"):
         #             print(file)
         #             with open(location + '\\' + file) as f:
         #                 self.data.append(json.load(f))
         #     except Exception as e:
         #         raise e
 
+        folder = location
+        location = os.getcwd() + '\\data\\' + location
         for file in os.listdir(location):
             try:
-                if file.endswith(file_name + ".json"):
-                    print(file)
+                if file.endswith(".json"):
+                    print('Working on: ' + file + ' --------------------')
+                    self.file_output_name = 'imgs/' + folder + ' - ' + file[0:len(file)-5] + '.png'
+                    print(self.file_output_name)
                     with open(location + '\\' + file) as f:
                         self.data.append(json.load(f))
+
+                    self.data = self.data[0]
+                    self.lower_star_filtration()
+                    self.data.clear()
             except Exception as e:
                 raise e
 
-        self.data = self.data[0]
+        # self.data = self.data[0]
 
     def find_extrema(self):
         """
@@ -138,6 +160,63 @@ class LineSpike:
         """
         Implements lower-star filtration on the data set to generate a merge tree and persistence diagram.
         """
+        N = len(self.data)  # The number of points
+        t = np.linspace(0, N, N)
+        x = self.data
+        # x = np.cos(2*np.pi*t) + t
+        # self.data = x
+        # x = math.pow(t, 3)
+
+        # plt.plot(t, x)
+        # plt.title("$\\cos(2 pi t) + t$")
+        # plt.xlabel("t")
+        # plt.show()
+
+        # Add edges between adjacent points in the time series, with the "distance"
+        # along the edge equal to the max value of the points it connects
+        I = np.arange(N - 1)
+        J = np.arange(1, N)
+        V = np.maximum(x[0:-1], x[1::])
+        # Add vertex birth times along the diagonal of the distance matrix
+        I = np.concatenate((I, np.arange(N)))
+        J = np.concatenate((J, np.arange(N)))
+        V = np.concatenate((V, x))
+        # Create the sparse distance matrix
+        D = sparse.coo_matrix((V, (I, J)), shape=(N, N)).tocsr()
+        dgm0 = ripser(D, maxdim=0, distance_matrix=True)['dgms'][0]
+        dgm0 = dgm0[dgm0[:, 1] - dgm0[:, 0] > 1e-3, :]
+        allgrid = np.unique(dgm0.flatten())
+        allgrid = allgrid[allgrid < np.inf]
+        xs = np.unique(dgm0[:, 0])
+        ys = np.unique(dgm0[:, 1])
+        ys = ys[ys < np.inf]
+
+        # Plot the time series and the persistence diagram
+        plt.figure(figsize=(12, 6))
+        ylims = [min(self.data) - 1, max(self.data) + 1]
+        # ylims = [-1, 6.5]
+        plt.subplot(121)
+        plt.plot(t, x)
+        ax = plt.gca()
+        ax.set_yticks(allgrid)
+        ax.set_xticks([])
+        plt.ylim(ylims)
+        # plt.grid(linewidth=1, linestyle='--')
+        plt.title("Data")
+        plt.xlabel("t")
+
+        plt.subplot(122)
+        ax = plt.gca()
+        ax.set_yticks(ys)
+        ax.set_xticks(xs)
+        plt.ylim(ylims)
+        # plt.grid(linewidth=1, linestyle='--')
+        plot_diagrams(dgm0, size=50)
+        plt.title("Persistence Diagram")
+
+        # plt.show()
+        plt.savefig(self.file_output_name)
+        plt.close()
 
     def generate_persistence_diagram(self):
         """
@@ -147,7 +226,17 @@ class LineSpike:
 
 def main():
     ls = LineSpike()
-    ls.setup('chi_homicide', 'weekly')
+    folders = ['astro', 'chi_homicide', 'climate_awnd', 'climate_prcp', 'climate_tmax',
+                    'eeg_10000', 'eeg_2500', 'eeg_500', 'flights', 'nz_tourist',
+                    'stock_price', 'stock_volume', 'unemployment']
+
+    for folder in folders:
+        ls.setup(folder)
+
+    # ls.setup('stock_volume', 'aapl_volume')
+    # ls.setup('stock_price', 'aapl_price')
+
+    # ls.setup('lines', 'equals_1')
 
 
 if __name__ == '__main__':
